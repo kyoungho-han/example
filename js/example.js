7c2f8e5a2b04c4abe4c7c328a437f44d9bed0639
한경호 <hanko0096@gmail.com>
2022-12-08 14:43:06 +0900

    setUserName 설정

--- js/example.js
+++ js/example.js
@@ -1,18 +1,20 @@
 const nameH1Element = document.querySelector('h1.inline');
 const connectNameElement = document.querySelector('span.red');
+
+const setUserName = (name) => {
+  nameH1Element.textContent = name;
+  connectNameElement.textContent = name;
+};
+
 const localName = localStorage.getItem('name');
-if (localName) {
-  nameH1Element.textContent = localName;
-  connectNameElement.textContent = localName;
-}
+if (localName) setUserName(localName);
 
 nameH1Element.onclick = () => {
   const inputName = prompt('이름을 입력해 주세요.');
   if (inputName) {
     localStorage.setItem('name', inputName);
 
-    nameH1Element.textContent = inputName;
-    connectNameElement.textContent = inputName;
+    setUserName(inputName);
   } else {
     alert('이름이 입력되지 않았습니다.');
   }
